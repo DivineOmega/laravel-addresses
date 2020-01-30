15769b51dbd420642f64b18de93f7ab093777a77
Jordan Hall <jordan@langleyfoxall.co.uk>
2020-01-30 12:12:48 +0000

    Publish/merge config

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -10,6 +10,14 @@ class ServiceProvider extends BaseServiceProvider
 {
     public function boot()
     {
+        $this->publishes([
+            __DIR__.'/config/addresses.php' => config_path('addresses.php'),
+        ]);
+
+        $this->mergeConfigFrom(
+            __DIR__.'/config/addresses.php', 'addresses'
+        );
+
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
 
         Address::observe(AddressObserver::class);
